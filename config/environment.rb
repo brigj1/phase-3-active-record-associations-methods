4fc3f9d6483db67ef6a5563ded9044cc84575740
Brigham <brig.johnson@gmail.com>
2022-06-16 06:30:36 -0400

    commit tree 9b73905b36da65db4f23bbc7fc4be2c83f3ac748

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,9 @@ require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
 require_all 'app/models'
+
+def reload
+    load "app/models/artist.rb"
+    load "app/models/genre.rb"
+    load "app/models/song.rb"
+end
